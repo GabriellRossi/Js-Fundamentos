429858df1d886f18dd5b59a9317899ae45333269
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-17 23:30:02 -0300

    Declarando Variáveis

---
+++ 02-Declarando-Variáveis/truthy-falsy.js
@@ -0,0 +1,19 @@
+const usuarioLogado = true
+const contaPaga = false
+
+console.log(1 == usuarioLogado);
+console.log(0 == contaPaga);
+
+const salario = 12
+const funcionario = "Pedro"
+
+console.log(typeof salario);
+console.log(typeof funcionario);
+
+
+let minhaVar;
+
+let minhaVar2 = null
+
+console.log(typeof minhaVar);
+console.log(typeof minhaVar2);
