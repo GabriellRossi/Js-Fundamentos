d20794c8ce7b0cb162772cc303632dca704fc04f
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-17 23:30:32 -0300

    Tipos de erros do console

---
+++ 03-Javascript-E-Nodejs/erros.js
@@ -0,0 +1,10 @@
+const numero;
+
+console.log(245);
+console.log("Eu sou um texto");
+console.log(245);
+
+console.log("Deu erro");
+console.error("Deu erro");
+
+console.error(new Error("Deu Erro"))
