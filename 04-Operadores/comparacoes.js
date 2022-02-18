1f8fee9115b953c8869f4321d54d1dfbca5322db
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-17 23:31:34 -0300

    Comparadores, Template-String, Ternario

---
+++ 04-Operadores/comparacoes.js
@@ -0,0 +1,34 @@
+// COMPARAÇÃO IMPLÍCITA
+
+const numero = 5
+const texto = "5"
+
+console.log(numero == texto);
+
+//TYPEOF
+console.log(typeof numero);
+console.log(typeof texto);
+
+/* TIPOS DE COMPARAÇÕES
+// = Só atribui o valor de uma variável
+
+// == Só compara o valor
+
+// === Compara o valor e o tipo de dados, exemplo, String, Number, Boolean etc...
+
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+
+// != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false. 
+*/ // FIM DO TIPOS DE COMPARAÇÃO
+
+//Conversão Explícita 
+
+Number()  // Tipos Primitivos
+String()   // Tipos Primitivos
+Boolean()  // Tipos Primitivos
+
+
+// LINK DA DOCUMENTAÇÃO DE EXPRESSÕES E OPERADORES: 
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
---
+++ 04-Operadores/template-string.js
@@ -0,0 +1,18 @@
+//TEMPLATE STRING
+// const nome = "Gabriel"
+// const idade = 2022-1933
+// const cidadeDeNascimento = "São Paulo"
+// const apresentacao = `Meu nome é ${nome}, minha idade é ${idade}, e nasci na cidade de ${cidadeDeNascimento}`
+
+// console.log(apresentacao);
+
+// EXERCICIO 
+
+const nome = "Gabriel"
+const idade = 24
+const bebidaMaior = "cerveja"
+const bebidaMenor = "suco"
+
+const pedido = `${nome} diz: 'por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}'`
+
+console.log(pedido);
